9d96d6fc686d228c086c8eaebc69036039ed24e5
Mark <mark.zhang@tpisoftware.com>
2022-07-29 14:13:58 +0800

    帳號功能 - 登入

--- src/main/java/com/mark/springbootmall/controller/UserController.java
+++ src/main/java/com/mark/springbootmall/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.mark.springbootmall.controller;
 
+import com.mark.springbootmall.dto.UserLoginRequest;
 import com.mark.springbootmall.dto.UserRegisterRequest;
 import com.mark.springbootmall.model.User;
 import com.mark.springbootmall.service.UserService;
@@ -25,5 +26,12 @@ public class UserController {
         User user = userService.getUserById(userId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
--- src/main/java/com/mark/springbootmall/service/UserService.java
+++ src/main/java/com/mark/springbootmall/service/UserService.java
@@ -1,6 +1,7 @@
 package com.mark.springbootmall.service;
 
 import com.mark.springbootmall.dao.UserDao;
+import com.mark.springbootmall.dto.UserLoginRequest;
 import com.mark.springbootmall.dto.UserRegisterRequest;
 import com.mark.springbootmall.model.User;
 import lombok.RequiredArgsConstructor;
@@ -31,5 +32,21 @@ public class UserService {
 
     public User getUserById(Integer userId) {
         return userDao.getUserById(userId);
+    }
+
+    public User login(UserLoginRequest userLoginRequest) {
+        User user = userDao.getUserByEmail(userLoginRequest.getEmail());
+
+        if (user == null) {
+            log.warn("該 email {} 尚未註冊", userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+            return user;
+        } else {
+            log.warn("email {} 的密碼不正確",userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
