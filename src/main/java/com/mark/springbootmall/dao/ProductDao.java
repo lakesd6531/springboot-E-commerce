42d29ab59e0d2b8139bd15193c8978c713e620b5
Mark <mark.zhang@tpisoftware.com>
2022-08-08 10:35:17 +0800

    訂單功能 - 檢查 user 是否存在、扣除商品庫存

--- src/main/java/com/mark/springbootmall/dao/ProductDao.java
+++ src/main/java/com/mark/springbootmall/dao/ProductDao.java
@@ -132,6 +132,18 @@ public class ProductDao {
         return namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
     }
 
+    public void updateStock(Integer productId, Integer stock) {
+        String sql = "UPDATE product SET stock = :stock, last_modified_date = :lastModifiedDate" +
+            " WHERE product_id = :productId ";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
+        map.put("stock", stock);
+        map.put("lastModifiedDate", new Date());
+
+        namedParameterJdbcTemplate.update(sql, map);
+    }
+
     private String addFilterSql(String sql, Map<String, Object> map, ProductQueryParams productQueryParams) {
         if (productQueryParams.getCategory() != null) {
             sql = sql + " AND category = :category";
--- src/main/java/com/mark/springbootmall/service/OrderService.java
+++ src/main/java/com/mark/springbootmall/service/OrderService.java
@@ -2,14 +2,20 @@ package com.mark.springbootmall.service;
 
 import com.mark.springbootmall.dao.OrderDao;
 import com.mark.springbootmall.dao.ProductDao;
+import com.mark.springbootmall.dao.UserDao;
 import com.mark.springbootmall.dto.BuyItem;
 import com.mark.springbootmall.dto.CreateOrderRequest;
 import com.mark.springbootmall.model.Order;
 import com.mark.springbootmall.model.OrderItem;
 import com.mark.springbootmall.model.Product;
+import com.mark.springbootmall.model.User;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +23,41 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+    private final static Logger log = LoggerFactory.getLogger(OrderService.class);
 
     private final OrderDao orderDao;
     private final ProductDao productDao;
+    private final UserDao userDao;
 
     @Transactional
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        // 檢查 user 是否存在
+        User user = userDao.getUserById(userId);
+
+        if (user == null) {
+            log.warn("該 userId {} 不存在", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         int totalAmount = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
 
         for (BuyItem buyItem : createOrderRequest.getBuyItemList()) {
             Product product = productDao.getProductById(buyItem.getProductId());
+
+            // 檢查 product 是否存在、庫存是否足夠
+            if (product == null) {
+                log.warn("商品 {} 不存在", buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+            } else if (product.getStock() < buyItem.getQuantity()) {
+                log.warn("商品 {} 庫存數量不足，無法購買。剩餘庫存 {}，欲購買數量 {}",
+                    buyItem.getProductId(), product.getStock(), buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            // 扣除商品庫存
+            productDao.updateStock(product.getProductId(), product.getStock() - buyItem.getQuantity());
 
             // 計算總價錢
             int amount = buyItem.getQuantity() * product.getPrice();
